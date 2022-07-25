e3d2654952fe896428985764fc70fced10223eea
S-YC <s9292909@gmail.com>
2022-07-25 21:59:11 +0900

    채팅서버 웹 배포

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './Messages.css';
-import ScrollToBottom, {
-  useScrollToBottom,
-  useSticky,
-} from 'react-scroll-to-bottom';
+import ScrollToBottom from 'react-scroll-to-bottom';
 import Message from './Message/Message';
 
 const Messages = ({ messages, name }) => {
-  const scrollToBottom = useScrollToBottom();
-  const [sticky] = useSticky();
-
   return (
     <ScrollToBottom className="messages">
       {messages.map((message, i) => (
